0a1987e84cdc701cd75df852fe33c783d3e1b3c8
melvinRenversez <163991271+louanMel@users.noreply.github.com>
2024-04-22 12:48:50 +0200

    a

--- test/test3.py
+++ test/test3.py
@@ -1,10 +1,15 @@
 x = input('~# ')
 xs = x.split(' ')
 list_action = []
+print(xs)
 for r in xs:
     if r != '':
         if r[0] == '-':
-            print("find ::: ", r[1:])
+            print("find ::: ", r)
             print("index ::: ", xs.index(r))
             print("action ::: ", xs[xs.index(r)+1])
-print(xs)
+            val = [r, xs[xs.index(r)+1]]
+            list_action.append(val)
+            
+print('')
+print(list_action)
